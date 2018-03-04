ea7c0b719133fe94a652fee430bcdcacd47e58d6
fangshibo <41534631@qq.com>
2018-03-04 18:38:32 +0800

    button

--- my.js
+++ my.js
@@ -2,7 +2,7 @@
 * @Author: Marte
 * @Date:   2018-02-25 12:09:59
 * @Last Modified by:   Marte
-* @Last Modified time: 2018-03-04 15:51:21
+* @Last Modified time: 2018-03-04 18:36:24
 */
 
 'use strict';
@@ -10,12 +10,13 @@
 
 
 $(document).ready(function($) {
-for (var i = 1; i <= 20; i++) {
+for (var i = 1; i <=63; i++) {
             // 这里指定 id = "div" + i，对应为 div1 div2 div3
             var id = "div" + i;
-            var div = $("<div class =aa id='" + id + " '><div class=row><div class= col-md-6 >" + id +"</div> <div class= col-md-6>" + id +"</div><div class= col-md-1 ></div><div class=col-md-10 ><p>" + id +"中华人民共和国</p></div><div class= col-md-6 >" + id +"</div><div class= col-md-6 >" + id +"</div></div></div>");
-            $("body").append(div);// body 拼接一个 div
-
+            var div = $("<div class =aa id='" + id + "'><div class=row><div class= col-md-6 >" + id +"</div> <div class= col-md-6>" + id +"</div><div class= col-md-1 ></div><div class=col-md-10 ><p>" + id +"中华fgjhghjgjghjgdsa共和国</p></div><div class= col-md-6 >" + id +"</div><div class= col-md-6 >" + id +"</div></div></div>");
+            $("#ddd").append(div);// body 拼接一个 div
+                $(".col-md-10").height(70);
+                //$(".col-md-10").margin(3);
         }
 
 });
